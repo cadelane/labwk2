079905ad29d8de6d9db466dd0cac225d019a8123
Cade Lane <c3235803@uon.edu.au>
2020-02-25 14:47:15 +1100

    initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()  {
+    float x;
+    float y;
+    printf("Enter a number:  ");
+    scanf("%f", &x);
+    y=9*x+32 ;
+    printf("y:  %f\n", y);
+    return 0;
+    }
